fa13395cbde7d52508d7d39d870a63ad3eea176a
Your Name <you@example.com>
2020-10-09 03:37:50 +0800

    Updates

---
+++ precache-manifest.0efb8ea60a65296e8baba276532eda1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbba15c4c1383b02b75cefb722fef620",
+    "url": "/monstafriends/index.html"
+  },
+  {
+    "revision": "ebf29d02b3ba4d173f9b",
+    "url": "/monstafriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3d70db17b947d11f97d2",
+    "url": "/monstafriends/static/css/main.d118e313.chunk.css"
+  },
+  {
+    "revision": "ebf29d02b3ba4d173f9b",
+    "url": "/monstafriends/static/js/2.209d094b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstafriends/static/js/2.209d094b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d70db17b947d11f97d2",
+    "url": "/monstafriends/static/js/main.0c589674.chunk.js"
+  },
+  {
+    "revision": "87eaa8513b36628064f3",
+    "url": "/monstafriends/static/js/runtime-main.be5fe38b.js"
+  },
+  {
+    "revision": "6c2ae7dc1e754d34f0db4593aafd6d3f",
+    "url": "/monstafriends/static/media/MR.MONSTAR.6c2ae7dc.woff"
+  }
+]);
